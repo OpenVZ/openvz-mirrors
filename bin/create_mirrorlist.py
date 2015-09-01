e19e4e96ec726eaa149ece75a028997987463747
Konstantin <wolf@parallels.com>
2015-09-01 14:00:04 +0300

    Skip disabled mirrorlists, see #PSBM-39190

--- bin/create_mirrorlist.py
+++ bin/create_mirrorlist.py
@@ -5,13 +5,15 @@ import sys
 import re
 
 url_patterns = ["OVZ-http: "]
+state_pattern = "State:"
+state_enabled_pattern = "%s Enabled" % state_pattern
 
 # Check for parameter
 try:
     git_mirrors = sys.argv[1]
     repo = sys.argv[2]
 except IndexError:
-    sys.stderr.write("Usage: %s %mirrors.masterlist %path_to_repo\n" % sys.argv[0])
+    sys.stderr.write("Usage: %s $mirrors.masterlist $path_to_repo\n" % sys.argv[0])
     sys.exit(1)
 
 try:
@@ -24,6 +26,8 @@ except:
 
 mirrorlist_urls = ["http://download.openvz.org/"]
 
+check_state = False
+
 for line in data:
     line = line.rstrip('\n')
     our_url = False
@@ -32,9 +36,14 @@ for line in data:
             continue
         our_url = True
         break
+    if check_state and line.startswith(state_pattern):
+        if not line.startswith(state_enabled_pattern):
+            del mirrorlist_urls[len(mirrorlist_urls)-1]
+        check_state = False
     if not our_url:
         continue
     mirrorlist_urls.append("http://" + re.sub("^.*: ", "", line))
+    check_state = True
 
 for url in mirrorlist_urls:
     print url + repo
